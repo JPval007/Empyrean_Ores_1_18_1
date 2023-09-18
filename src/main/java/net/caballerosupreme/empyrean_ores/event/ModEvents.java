c973645c5f459963319fae770fea62d77cdf0ad7
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-09-18 13:06:13 -0600

    Fixed Mod Events and figured out how to send system messages

--- src/main/java/net/caballerosupreme/empyrean_ores/event/ModEvents.java
+++ src/main/java/net/caballerosupreme/empyrean_ores/event/ModEvents.java
@@ -1,39 +1,35 @@
 package net.caballerosupreme.empyrean_ores.event;
 
 import net.caballerosupreme.empyrean_ores.EmpyreanOres;
-import net.minecraft.client.particle.CritParticle;
-import net.minecraft.client.particle.ExplodeParticle;
-import net.minecraft.client.particle.FireworkParticles;
-import net.minecraft.core.particles.*;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
+import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.awt.*;
+import java.util.UUID;
+
 
 /*
  * Use events when you don't have access to the classes
  */
-@Mod.EventBusSubscriber(modid = EmpyreanOres.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = EmpyreanOres.MOD_ID)
 public class ModEvents {
     //This is the Mod bus event where you can do anything using events
     //Don't forget to subscribe them to the bus
     //Test Event
-/*
     @SubscribeEvent
-    public static void onDamageTaken(LivingHurtEvent event){
-        LivingEntity entity = event.getEntityLiving();
-        Level level = entity.level;
+    public static void onLivingHurt(LivingHurtEvent event){
+        if (event.getEntity() instanceof Sheep){
+            if (event.getSource().getDirectEntity() instanceof Player player){
+                //Send a System Message prior to 1.19+ forge
+                player.sendMessage(new TextComponent("Punched a Sheep!"), UUID.randomUUID());
 
-        if (event.getSource() == DamageSource.LIGHTNING_BOLT){
-
-            level.addParticle(DustParticleOptions.REDSTONE, entity.getX(),entity.getY(), entity.getZ(),0.0D,0.3D,0.0D);
+            }
         }
-
     }
-*/
 
 }
